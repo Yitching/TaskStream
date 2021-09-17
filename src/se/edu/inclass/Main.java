2f47c8909cd5a2a0372d946efa380c2eabc31a7e
Yitching <seetohyitching@gmail.com>
2021-09-17 17:17:33 +0800

    Add all code

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -17,11 +17,15 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        System.out.println("Printing deadlines");
-        printDeadlines(tasksData);
-
+//        System.out.println("Printing deadlines");
+//        printDeadlines(tasksData);
+//
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
-
+//        printData(tasksData);
+//        printDataWithStreams(tasksData);
+        printDeadlinesUsingStream(tasksData);
+        System.out.println("Total number of deadlines (using stream): " +
+                countDeadlinesUsingStream(tasksData));
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
